83af5a0c3a1d91373083f8cb64b9accd65cfb333
Nadia Cobos Zaleta <nadia.cobos.zaleta@accenture.com>
2025-10-22 11:29:34 -0600

    Sesion 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
+import type { JSX } from 'react'
 import './App.css'
+import PrintMovie from './components/PrintMovie'
+import { mockMovie, movieList } from './mocks/movies.mocks'
+/*
+function PrintMovie(): JSX.Element{
+  return(
+    <>
+      <h3>ID: 512</h3>
+      <h3>Title: Citizen Kane</h3>
+      <h3>Descripcion: Esta es uuna pelicula antigua</h3>
+      <h3>pictureUrl: ruta de la imagen</h3>
+      <h3>releaseDate: 1987-10-05</h3>
+    </>
+  )
+}*/
 function App() {
  return (
    <>
+    <h1>Catálogo de Peliculas Accenture</h1>
+    <hr/>
+    <h1><PrintMovie movie={mockMovie}/></h1>    
+
+  <h1>Catálogo en Json </h1>
+   {JSON.stringify(movieList)}
+
+    <h1>Movies mayor a 2024 </h1>
+    {
+      movieList
+      .filter((movie) => movie.releaseDate.getFullYear() >= 2024 )
+      .map((movie) => (
+        <PrintMovie key={movie.id} movie = {movie} />
+      )
+      )}
+
+    <h1>Movies con palabra clave </h1>
+      {movieList.map((movie) => (
+            movie.description.includes("to") ? (<PrintMovie key={movie.id} movie={movie} />) : null
+       
+       
+        ))}
+
    </>
+   
  )
 }
 
