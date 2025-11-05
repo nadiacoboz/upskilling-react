9ed0707981833f591c8eb1f98a9ac213d2868aaa
Nadia Cobos Zaleta <nadia.cobos.zaleta@accenture.com>
2025-11-05 11:00:58 -0600

    sesion-06-routes final

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,18 @@
-//import type { JSX } from 'react'
+import { useRoutes } from 'react-router-dom'
 import './App.css'
-//import PrintMovie from './components/PrintMovie/PrintMovie'
-import MovieList from './components/PrintMovie/MovieList'
-//import UserForm from './components/userForm/UserForm'
-//import {mockMovie, movieList } from './mocks/movies.mocks'
-/*
-function PrintMovie(): JSX.Element{
-  return(
-    <>
-      <h3>ID: 512</h3>
-      <h3>Title: Citizen Kane</h3>
-      <h3>Descripcion: Esta es uuna pelicula antigua</h3>
-      <h3>pictureUrl: ruta de la imagen</h3>
-      <h3>releaseDate: 1987-10-05</h3>
-    </>
-  )
-}*/
+import { appRoutes } from './routes/routes';
+import { Suspense } from 'react';
+
 function App() {
+  const routing = useRoutes(appRoutes);
  return (
    <>
     <h1>Catálogo de Peliculas Accenture</h1>
     <hr/>
-    {/* <h1><PrintMovie movie={mockMovie}/></h1>    
- 
-  <h1>Catálogo en Json </h1>
-   {JSON.stringify(movieList)}
- 
-    <h1>Movies mayor a 2024 </h1>
-    {
-      movieList
-      .filter((movie) => movie.releaseDate.getFullYear() >= 2024 )
-      .map((movie) => (
-        <PrintMovie key={movie.id} movie = {movie} />
-      )
-    )}
- 
-      <h1>Movies con palabra clave </h1>
-      {movieList.map((movie) => (
-            movie.description.includes("to") ? (<PrintMovie key={movie.id} movie={movie} />) : null
-       
-       
-        ))} */}
-
-      {/* <MovieList movies = {movieList}/> */
-        <MovieList />
-      }
- 
-      {/* <div>
-        <h2>Formularios del Usuario:</h2>
-        <UserForm />
-      </div>  */}
-   </>
-   
+    <Suspense fallback={<p>Loading...</p>}>{routing}</Suspense>
+    
+   </>   
  )
 }
- 
+
 export default App
