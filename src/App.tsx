3f9a405b3b8b0c992fb6f85ca1191cbdfd2beec0
Nadia Cobos Zaleta <nadia.cobos.zaleta@accenture.com>
2025-11-12 15:55:01 -0600

    adding mocks: Update existing files with new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import "./App.css";
 import PrintMovie from "./components/PrintMovie/PrintMovie";
+import { mockMovie } from "./mocks/movie.mocks";
 
 function App() {
   return (
-    <>
+    <>      
+      <h1>Catálogo de Peliculas Acceture</h1>
+      <hr/>
+      <h1><PrintMovie movie={mockMovie} /></h1>
+
+      {        
+      /* 
       <h1><PrintMovie movie={{
         id: 1,
         title: 'Citizen Kane',
         descripcion: 'This film examines the life of Kane',
         pictureUrl: 'imageURL',
         releaseDate: new Date('1941-10-25')
-      }} /></h1>
+        }} />
+       </h1> 
+       */
+      }
 
       {/* <h1><PrintMovieTitle title="Citizen Kane" /></h1> */}
     </>
