307690aa63ffc353ec9b87358f27319d799416c6
Nadia Cobos Zaleta <nadia.cobos.zaleta@accenture.com>
2025-11-12 15:29:38 -0600

    adding interfaces: Update existing files with new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,20 @@
-import type { JSX } from "react";
 import "./App.css";
-
-const name: string = "Hola Luis!";
-
-function PrintComponent(): JSX.Element {
-  return <h3>Mi primer componente</h3>;
-}
-
-function PrintMovie(): JSX.Element {
-  return (
-    <>
-      <h3>ID: 512</h3>
-      <h3>Title: Citizen Kane</h3>
-      <h3>Descripcion: Esta es uuna pelicula antigua</h3>
-      <h3>pictureUrl: ruta de la imagen</h3>
-      <h3>releaseDate: 1987-10-05</h3>
-    </>
-  );
-}
+import PrintMovie from "./components/PrintMovie/PrintMovie";
 
 function App() {
   return (
     <>
-      Hola React!
+      <h1><PrintMovie movie={{
+        id: 1,
+        title: 'Citizen Kane',
+        descripcion: 'This film examines the life of Kane',
+        pictureUrl: 'imageURL',
+        releaseDate: new Date('1941-10-25')
+      }} /></h1>
 
-      <p>Hola Mundo</p>
-      <br />
-      
-      {/* Mostrar informacion en react */}
-      {5 + 7 + 7 + 8}
-      <br />
-      {"5+64+4"}
-      <br />
-
-      {/* Renderizar informacion */}      
-      <p>{"Hola compañero: "}{name}</p>
-      <p>{`Hola compañero: ${name}`}</p>
-      
-      <h1>
-        <PrintComponent />
-      </h1>
-      
-      <br />
-      <h1><PrintMovie /></h1>
-      <h1><PrintMovie /></h1>
-      <h1><PrintMovie /></h1>
-      <h1><PrintMovie /></h1>
-      <h1><PrintMovie /></h1>
+      {/* <h1><PrintMovieTitle title="Citizen Kane" /></h1> */}
     </>
   )
 }
 
-export default App;
+export default App
