a59dbaf2f8d2cb9e3189af68117ae64fe3a9b60e
Nadia Cobos Zaleta <nadia.cobos.zaleta@accenture.com>
2025-11-20 11:28:32 -0600

    UseContext

--- src/components/PrintMovie/MovieList.tsx
+++ src/components/PrintMovie/MovieList.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState, type JSX } from "react";
 import type { MovieListProps } from "./types";
 import PrintMovie from "./PrintMovie";
 import type { Movie } from "../../types/movie";
+import useUser from "../../hooks/useUser";
 
 function MovieList({movies}: MovieListProps): JSX.Element{
     const [showRecent, setShowRecent] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(true);
     const [movieData, setMovieData] = useState<Movie[]>([]);
+    const {username} = useUser();
 
     useEffect(() => {
         setTimeout(() => {
@@ -30,6 +32,10 @@ function MovieList({movies}: MovieListProps): JSX.Element{
                 { showRecent ? "Mostrar todas las peliculas" : "Mostrar las recientes" }
             </button>
 
+            <p>
+                Bienvenido {username}!
+            </p>
+            
             {filteredMovies.length > 0 
             ? (filteredMovies.map((movie: Movie) => (
                 <PrintMovie key = {movie.id} movie={movie} />
